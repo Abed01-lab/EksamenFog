f7f34d3b188ab155c63af028a9e00988e79ada44
Gustav <cph-gw30@cphbusiness.dk>
2020-05-06 16:33:05 +0200

    calc

--- src/main/java/DBAccess/StyklisteMapper.java
+++ src/main/java/DBAccess/StyklisteMapper.java
@@ -1,19 +1,132 @@
 package DBAccess;
 
-import FunctionLayer.Carport;
-import FunctionLayer.Materials;
-import FunctionLayer.Skur;
-import FunctionLayer.Tag;
+import FunctionLayer.*;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import static DBAccess.CarportMapper.createOrdre;
+import static FunctionLayer.CalculatorSecond.udregnStyklisterFladt;
 
 public class StyklisteMapper {
 
-    public static List<Materials> getFladtStyklister(int kvm) throws SQLException, ClassNotFoundException {
+    public static List<Materials> getStyklister() throws SQLException, ClassNotFoundException {
+        ArrayList<Materials> materialer = new ArrayList<>();
+        String SQL = "SELECT * FROM fogprojekt.styklisteitems";
+
+        try {
+            Connection con = Connector.connection();
+
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+
+                int id = rs.getInt("itemId");
+                String beskrivelse = rs.getString("beskrivelse");
+                String enhed = rs.getString("enhed");
+                int pris = rs.getInt("pris");
+
+                Materials materialInstance = new Materials(id, beskrivelse, enhed, pris);
+                materialer.add(materialInstance);
+
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return materialer;
+    }
+
+    public static void opdaterMateriale(Materials materiale) {
+        try {
+            Connection con = Connector.connection();
+            String SQL = "INSERT INTO fogprojekt.styklisteitems (beskrivelse, enhed, pris) VALUES (?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, materiale.getBeskrivelse());
+            ps.setString(2, materiale.getEnhed());
+            ps.setInt(3, materiale.getPris());
+            ps.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void lavStyklisterTilCarport(Carport carport, Tag tag, Skur skur, int stolper, int areal, int spær) {
+        int ordreId = 0;
+        int serienummer = 0;
+
+        try {
+            ordreId = createOrdre(carport, tag, skur);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            List<Materials> materialer = getStyklister();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        ArrayList<CalculatedItems> liste = udregnStyklisterFladt(carport, tag, skur);
+
+        for(int i = 0; i < liste.size(); i++) {
+            String item = liste.get(i).getItemNavn();
+
+            switch(item){
+                case "breddestolper":
+                    serienummer = 1;
+                    break;
+                case "længdestolper":
+                    serienummer = 1;
+                case "stolper":
+                    serienummer = 6;
+                case "spær":
+                    serienummer = 4;
+            }
+
+            try {
+                Connection con = Connector.connection();
+                String SQL = "INSERT INTO fogprojekt.stykliste (ordreId, serienummer, antal, længde) VALUES (?, ?, ?, ?";
+                PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+                ps.setInt(1, ordreId);
+                ps.setInt(2, serienummer);
+                ps.setDouble(3, liste.get(i).getItemAntal());
+                ps.setDouble(4, liste.get(i).getItemLængde());
+                ps.executeUpdate();
+
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            Connection con = Connector.connection();
+            String SQL = "INSERT INTO fogprojekt.stykliste (ordreId, serienummer, antal, længde) VALUES (?, ?, ?, ?";
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+
+            //serienummer = tag.getTagmateriale();
+
+            ps.setInt(1, ordreId);
+            ps.setInt(2, serienummer);
+            ps.setDouble(3, areal);
+            ps.setDouble(4, 0);
+            ps.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+/*public static List<Materials> getFladtStyklister(int kvm) throws SQLException, ClassNotFoundException {
         ArrayList<Materials> materialer = new ArrayList<>();
         String SQL = "SELECT * FROM fogprojekt.fladttagstyklister";
 
@@ -46,71 +159,12 @@ public class StyklisteMapper {
             e.printStackTrace();
         }
         return materialer;
-    }
-
-    public static List<Materials> getStyklister() throws SQLException, ClassNotFoundException {
-        ArrayList<Materials> materialer = new ArrayList<>();
-        String SQL = "SELECT * FROM fogprojekt.styklisteitems";
-
-        try {
-            Connection con = Connector.connection();
-
-            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-
-                int id = rs.getInt("itemId");
-                String beskrivelse = rs.getString("beskrivelse");
-                String enhed = rs.getString("enhed");
-                int pris = rs.getInt("pris");
-
-                Materials materialInstance = new Materials(id, beskrivelse, enhed, pris);
-                materialer.add(materialInstance);
-
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return materialer;
-    }
+    }*/
 
 
-    public static void lavStyklisterTilCarport(Carport carport, Tag tag, Skur skur, int stolper, int areal, int spær) {
-        try {
-            int ordreId = createOrdre(carport, tag, skur);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
 
-        try {
-            List<Materials> materialer = getStyklister();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        for(int i = 0; i < 4; i++) {
-            try {
-                Connection con = Connector.connection();
-                String SQL = "INSERT INTO fogprojekt.stykliste () VALUES (?, ?, ?)";
-                PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
-
-                ps.executeUpdate();
-
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-
-    /*public static List<Materials> getSkråStyklister(int kvm) throws SQLException, ClassNotFoundException {
+    /*
+    public static List<Materials> getSkråStyklister(int kvm) throws SQLException, ClassNotFoundException {
         ArrayList<Materials> materialer = new ArrayList<>();
         String SQL = "SELECT * FROM fogprojekt.skråtagstyklister";
 
@@ -141,20 +195,41 @@ public class StyklisteMapper {
             e.printStackTrace();
         }
         return materialer;
-    }*/
+    }
 
-    public static void opdaterMateriale(Materials materiale) {
+   /* public static void opdaterMaterialeFladtTag(Materials materiale) {
         try {
             Connection con = Connector.connection();
-            String SQL = "INSERT INTO fogprojekt.styklisteitems (beskrivelse, enhed, pris) VALUES (?, ?, ?)";
+            String SQL = "INSERT INTO fogprojekt.fladttagstyklister (deminsion, længde, antal, enhed, beskrivelse) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, materiale.getBeskrivelse());
-            ps.setString(2, materiale.getEnhed());
-            ps.setInt(3, materiale.getPris());
+            ps.setString(1, materiale.getDeminsion());
+            ps.setFloat(2, materiale.getLængde());
+            ps.setFloat(3, materiale.getAntal());
+            ps.setString(4, materiale.getEnhed());
+            ps.setString(5, materiale.getBeskrivelse());
             ps.executeUpdate();
 
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
-}
+
+
+    public static void opdaterMaterialeSkråTag(Materials materiale) {
+        try {
+            Connection con = Connector.connection();
+            String SQL = "INSERT INTO fogprojekt.skråtagstyklister (deminsion, længde, antal, enhed, beskrivelse) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, materiale.getDeminsion());
+            ps.setFloat(2, materiale.getLængde());
+            ps.setFloat(3, materiale.getAntal());
+            ps.setString(4, materiale.getEnhed());
+            ps.setString(5, materiale.getBeskrivelse());
+            ps.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}*/
+
--- src/main/java/FunctionLayer/CalculatorSecond.java
+++ src/main/java/FunctionLayer/CalculatorSecond.java
@@ -1,5 +1,7 @@
 package FunctionLayer;
 
+import java.util.*;
+
 public class CalculatorSecond {
 
 
@@ -10,7 +12,7 @@ public class CalculatorSecond {
         }
         else {
             antalStolper = Math.ceil((carport.getLængde() / 325.0) );
-            if(antalStolper > 2){
+            if(antalStolper < 2){
                 antalStolper = 2;
             }
         }
@@ -28,7 +30,8 @@ public class CalculatorSecond {
     public static double beregnTagLægter(Carport carport, Tag tag){
         double skråTagLængde = Math.sqrt(((Math.pow(((carport.getBredde() / 2)) * Math.tan(Math.toRadians(tag.getHældning())), 2)) + (Math.pow((carport.getBredde() / 2), 2)))) + 30;
         skråTagLængde =- 65;
-        return Math.ceil(skråTagLængde / 30.7) + 1;
+        return (Math.ceil(skråTagLængde / 30.7) + 1) * 2;
+        // måske * 2
     }
 
     //public static double beregnAntalSpærSkråtVandret(Carport carport){
@@ -47,15 +50,18 @@ public class CalculatorSecond {
         return areal / 10000;
     }
 
-    public static void udregnStyklisterFladt(Carport carport, Tag tag, Skur skur){
-        double breddestolpe = 2;
-        double længdestolpe = 2;
-        double stolper = beregnAntalStolper(carport, skur);
+    public static ArrayList<CalculatedItems> udregnStyklisterFladt(Carport carport, Tag tag, Skur skur){
+        double stolperAntal = beregnAntalStolper(carport, skur);
+        double spærAntal = beregnAntalSpærFladt(carport);
         double areal = beregnTagAreal(carport, tag);
-        double spær = beregnAntalSpærFladt(carport);
 
+        ArrayList<CalculatedItems> list = new ArrayList<CalculatedItems>();
+        CalculatedItems breddestolpe = new CalculatedItems("breddestolpe", 2, carport.getBredde());
+        CalculatedItems længdestolpe = new CalculatedItems("længdestolpe", 2, carport.getLængde());
+        CalculatedItems stolper = new CalculatedItems("stolper", stolperAntal, carport.højde);
+        CalculatedItems spær = new CalculatedItems("spær", spærAntal, carport.bredde);
         //Mapper
-
+        return list;
     }
 
     public static void main(String[] args) {
