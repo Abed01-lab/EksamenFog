fbdcca35a50f6b3bd56c8f7630b04bf2e941bc85
David <josefsendavid@gmail.com>
2020-05-14 11:36:41 +0200

    svg pile

--- src/main/java/FunctionLayer/Svg.java
+++ src/main/java/FunctionLayer/Svg.java
@@ -10,7 +10,9 @@ public class Svg {
     private StringBuilder svg = new StringBuilder();
     private final String headerTemplate = "<svg  version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\"  xmlns:xlink=\"http://www.w3.org/1999/xlink\" height=\"%d\" width=\"%d\" viewBox=\"%s\" x=\"%d\" y=\"%d\" preserveAspectRatio=\"xMinYMin\">";
     private final String rectTemplate = "<rect x=\"%d\" y=\"%d\" height=\"%d\" width=\"%d\" style=\"stroke:#000000; fill: #ffffff\" />";
-    private final String arrowTemplate = "<defs> <marker id=\"beginArrow\" markerWidth=\"12\" markerHeight=\"9\" refX=\"0\" refY=\"6\" orient=\"auto\" <path d=\"M0,6 L12,0 L12,12 L0,6\" style=\"fill: #000000s;\" /> </marker>"
+    private final String labelTemplate = "<text style=\"text-anchor: middle\" x=\"%d\" y=\"%d\"> cm </text>"
+                                        + "<text style=\"text-anchor: middle\" transform=\"translate(\"%d\",\"%d\") rotate(-90)\"> 600 cm </text>";
+    private final String arrowTemplate = "<defs> <marker id=\"beginArrow\" markerWidth=\"12\" markerHeight=\"12\" refX=\"0\" refY=\"6\" orient=\"auto\"> <path d=\"M0,6 L12,0 L12,12 L0,6\" style=\"fill: #000000s;\" /> </marker>"
                                               + "<marker id=\"endArrow\" markerWidth=\"12\" markerHeight=\"12\" refX=\"12\" refY=\"6\" orient=\"auto\"> <path d=\"M0,0 L12,6 L0,12 L0,0\" style=\"fill: #000000;\" /> </marker> </defs>"
                                               + "<line x1=\"%d\"  y1=\"%d\" x2=\"%d\" y2=\"%d\" style=\"stroke:#006600; marker-start: url(#beginArrow); marker-end: url(#endArrow);\"/>";
 
@@ -31,8 +33,12 @@ public class Svg {
         svg.append(String.format(rectTemplate, x, y, height, width));
     }
 
-    public void addArrow(int x, int y, int x2, int y2){
+    public void addArrow(int x, int y, int x2, int y2) {
         svg.append(String.format(arrowTemplate, x, y, x2, y2));
+    }
+
+    public void addLabel(int x, int y, String afstand){
+        svg.append(String.format(labelTemplate, x, y, afstand));
     }
 
     public int getWidth() {
